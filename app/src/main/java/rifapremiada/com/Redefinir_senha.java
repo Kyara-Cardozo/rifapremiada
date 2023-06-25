57d2300504770640704b6eb20abaed21373391e2
Tiago <tiagojesilva7@gmail.com>
2023-06-25 12:19:39 -0300

    Adicionei uma mensagem quando clicar em recuperação de senha, além de abrir uma pagina para inserir o email, ao cliar em enviar, irá surgir uma mensagem informando que foi enviado com sucesso e voltará para a pagina principal de login.

--- app/src/main/java/rifapremiada/com/Redefinir_senha.java
+++ app/src/main/java/rifapremiada/com/Redefinir_senha.java
@@ -1,7 +1,11 @@
 package rifapremiada.com;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Redefinir_senha extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class Redefinir_senha extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_redefinir_senha);
+
+        Button voltarMain = findViewById(R.id.voltarMain);
+
+        voltarMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Código a ser executado quando o botão for clicado
+                Toast.makeText(getApplicationContext(), "Enviamos um email para recuperação!", Toast.LENGTH_LONG).show();
+
+                // Iniciar a nova atividade
+                Intent intent = new Intent(Redefinir_senha.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
+
+
+
